df421b4088d7d7e0e098a296d4cc6eb5c6045add
nickmzhang <nickmzhang@gmail.com>
2026-05-14 01:37:04 -0700

    Create index.json

---
+++ index.js
@@ -0,0 +1,9 @@
+{
+  "manifestVersion": 1,
+  "name": "nekoBT",
+  "id": "nekobt",
+  "version": "1.0.0",
+  "type": "torrent",
+  "icon": "https://nekobt.to/favicon.ico",
+  "code": "https://raw.githubusercontent.com/nickmzhang/hay_test/main/myexample.js"
+}
