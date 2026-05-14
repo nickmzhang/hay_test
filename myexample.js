92d551b85c018d98c1e570c1f1ce6b618f5375a2
nickmzhang <nickmzhang@gmail.com>
2026-05-14 01:57:34 -0700

    Update myexample.js

--- myexample.js
+++ myexample.js
@@ -1,21 +1,3 @@
-export default {
-  async test() {
-    // Used by Hayase to check if the extension is working
-    return true;
-  },
-
-  async single(query, options) {
-    // Main search function (single query)
-    return [];
-  },
-
-  async batch(queries, options) {
-    // Optional: batch search support
-    return [];
-  },
-
-  async movie(query, options) {
-    // Optional: movie-specific search (can return same as single for now)
-    return [];
-  }
-};
+export default async function search(query, options) {
+  return [];
+}
